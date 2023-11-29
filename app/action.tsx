96506f5f1698b5b438d37e69220b928dfd1fc36c
Ananth Narayan <narayanananth26@gmail.com>
2023-11-29 22:43:32 +0530

    Rename action.ts to action.tsx

--- app/action.tsx
+++ app/action.tsx
@@ -1,5 +1,7 @@
 "use server";
 
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 export const fetchAnime = async (page: number) => {
 	const res = await fetch(
 		`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
@@ -8,5 +10,7 @@ export const fetchAnime = async (page: number) => {
 	const data = await res.json();
 
 	console.log(data);
-	return data;
+	return data.map((item: AnimeProp, index: number) => (
+		<AnimeCard key={item.id} anime={item} index={index} />
+	));
 };
